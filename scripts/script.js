d81417c942c9cb67f3abfa0ef2cbc896944763e1
Calebe Oliveira <calebe81899026@gmail.com>
2022-12-19 20:32:49 -0300

    finished cronometer

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,67 @@
 let playBtn = document.getElementById('play').innerText = 'Play'
 let timer = document.getElementById('timer').innerText = '00:00'
 
-let sec = 0
+let seconds = 0
 let auxSec = 0
-let min = 0
+let minutes = 0
 let auxMin = 0
 
-let p0 
-let p1 
-let p2
-let p3 
-let p4
+let started = false
+let paused = false
+
+let cronometer
 
 function play_pause(){
 
-    p1 = setInterval(()=>{
-        auxMin++
-    },600000)
-    
-    p2 = setInterval(()=>{
-        min++
-    },60000)
-    
-    p3 = setInterval(()=>{
-        auxSec++
-    },10000)
-    
-    p4 = setInterval(()=>{
-        sec++
-    },1000)
-    
-    /** */
+    if(started == false){
 
-    p0 = setInterval(()=>{
-        document.getElementById('timer').innerText = `${auxMin > 5 ? auxMin = 0 : auxMin}${min > 9 ? min = 0 : min}:${auxSec > 5 ? auxSec = 0 : auxSec}${sec > 9 ? sec = 0 : sec}`
-    },1000)
+        started = true
+        sec = 0
+        cronometer = setInterval(()=>{
+            seconds++
+            document.getElementById('timer').innerText = (minutes < 10 ? '0' : '') + (seconds > 59 ? minutes++ && minutes : minutes) + ':' + 
+            (seconds > 59 ? seconds = 0 : seconds < 10 ? '0' + seconds : seconds)
+        },1000)
+    
+    } else {
+    
+        if(paused == false){
+
+            paused = true
+            clearInterval(cronometer)
+
+        } else {
+
+            paused = false
+            cronometer = setInterval(()=>{
+                seconds++
+                document.getElementById('timer').innerText = (minutes < 10 ? '0' : '') + (seconds > 59 ? minutes++ && minutes : minutes) + ':' + 
+                (seconds > 59 ? seconds = 0 : seconds < 10 ? '0' + seconds : seconds)
+            },1000)
+
+        }
+
+    }
+
+    if(paused === false){
+        document.getElementById('play').innerText = 'Pause'
+    } else {
+        document.getElementById('play').innerText = 'Play'
+    }
     
 }
 
 function stopTimer(){
     
-    clearInterval(p0)
-    clearInterval(p1)
-    clearInterval(p2)
-    clearInterval(p3)
-    clearInterval(p4)
+    started = false
+    paused = false
 
+    seconds = 0
+    minutes = 0 
+
+    document.getElementById('play').innerText = 'Play'
+    document.getElementById('timer').innerText = '00:00'
+
+    clearInterval(cronometer)
+   
 }
